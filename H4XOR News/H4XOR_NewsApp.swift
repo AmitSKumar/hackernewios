5f6834522245e1065b70f2de31cdb1a005198b1d
Amit Singh <amitsighbxr@gmail.com>
2023-09-07 18:41:20 +0800

    Initial Commit

---
+++ H4XOR News/H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NewsApp.swift
+//  H4XOR News
+//
+//  Created by user243065 on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
